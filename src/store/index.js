ae056a8430a35c1c41e45e9df9bde565691c2044
Daniel Padrino <danswarrior1@gmail.com>
2020-04-09 18:22:16 -0300

    saga config

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
 import { createStore, applyMiddleware, compose } from "redux"
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./reducers";
+import rootSaga from "./sagas";
 
 const initialState = {};
 
+//Get saga middleware object
+const sagaMiddleware = createSagaMiddleware();
+
 const middlewares = [
     //Middlewares go here
-
+    //..
+    sagaMiddleware,
 ];
 
-export default createStore(
+const store = createStore(
     rootReducer,
     initialState,
     compose(
@@ -17,3 +23,9 @@ export default createStore(
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
+
+//Run saga middleware
+sagaMiddleware.run(rootSaga);
+
+
+export default store;
